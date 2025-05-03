a5bb1c5decef48ca224956db6bccf6b50c0f7fdd
Volzanin25 <166922769+Frankfurt-Volzky@users.noreply.github.com>
2025-05-03 16:14:09 +0300

    Added main folder

---
+++ Web-site/js/mainpage.js
@@ -0,0 +1,13 @@
+let reg_button = document.querySelector(".log-in")
+let case = document.querySelector(".case")
+case.addEventListner("click",function(){
+
+})
+let FAQ = document.querySelector(".FAQ")
+FAQ.addEventListner("click",function(){
+    
+})
+let upgrade = document.querySelector(".upgrade")
+upgrade.addEventListner("click",function(){
+    
+})
