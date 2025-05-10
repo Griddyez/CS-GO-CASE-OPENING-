6acdc5890badc943c0f88745f22a963363d396ca
Volzanin25 <166922769+Frankfurt-Volzky@users.noreply.github.com>
2025-05-10 17:28:13 +0300

    Added language change

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,12 +1,12 @@
 let reg_button = document.querySelector(".log-in")
 let FAQ = document.querySelector(".FAQ")
-console.log("HIIIIIIIIIIIIIIIIIIIII")
 let upgrade = document.querySelector(".upgrade")
 let case1 = document.querySelector(".case")
 let rebalance = document.querySelector(".rebalance")
 let input = document.querySelector(".input")
 let login = document.querySelector(".log-in")
 let cases = document.querySelectorAll(".case-image")
+
 FAQ.addEventListener("click", function () {
     anime({
         targets: '.FAQ',
@@ -83,7 +83,7 @@ for (i = 0; i<cases.length;i++){
     let a = cases[i]
     a.addEventListener("click",()=>{
         anime({
-            targets: '.a',
+            targets: a,
             scale:[
                 {value:0.75,duration:300},
                 {value:1.0,duration:200},
@@ -94,3 +94,46 @@ for (i = 0; i<cases.length;i++){
         setTimeout(() => window.location.href = "./html/case.html", 500)
     })
 }
+var activities = document.querySelector(".selector");
+
+activities.addEventListener("click", function() {
+    var options = activities.querySelectorAll("option");
+    var count = options.length;
+    if(typeof(count) === "undefined" || count < 2)
+    {
+        addActivityItem();
+    }
+});
+let Contacts = document.querySelector(".Contancts")
+let Developers = document.querySelector(".Developers")
+let Followus = document.querySelector(".Follow-us")
+let dlist = document.querySelectorAll(".d-list-li")
+let dlist_r = ["Фёдор","Стас","Дмитрий"]
+let dlist_e = ["Fyodor","Stas","Dmitriy"]
+activities.addEventListener("change", function() {
+    if(activities.value == "Русский")
+    {
+        alert("YOU'VE CHANGED THE LANGUAGE TO COMMUNIST ONE, BE AWARE OF THIS UGLY CAPITALIST!")
+        rebalance.innerHTML = "Пополнение"
+        input.innerHTML = "Вывод"
+        Contacts.innerHTML = "Наши Контакты"
+        Developers.innerHTML = "Список Разработчиков"
+        Followus.innerHTML = "Следите за нашей активностью"
+        for (let i=0;i<dlist.length;i++){
+            dlist[i].innerHTML = dlist_r[i]
+        }
+    }
+    else if(activities.value="Английский")
+    {
+        alert("ВЫ ИЗМЕНИЛИ ЯЗЫК И ВЫ БОЛЬШЕ НЕ НА РУССКОЙ ВЕРСИИ САЙТА, УДАЧИ!")
+        rebalance.innerHTML = "Replenishment"
+        input.innerHTML = "Withdrawal"
+        Contacts.innerHTML = "Our Contacts"
+        Developers.innerHTML = "Developer list"
+        Followus.innerHTML = "Follow us"
+        for (let i=0;i<dlist.length;i++){
+            console.log(dlist[i])
+            dlist[i].innerHTML = dlist_e[i]
+        }
+    }
+});
